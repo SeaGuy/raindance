53d12b428c7172c2463a72bae2b7e27b6bc1d74c
Bill <wlaing@fau.edu>
2024-08-10 11:36:42 -0400

    added Swift code and UI work XCode

---
+++ src/SprinklerControlApp/SprinklerControlApp/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  SprinklerControlApp
+//
+//  Created by William E. Laing, Jr. on 8/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // IP address of your Arduino
+    let arduinoIP = "http://192.168.0.238" // Replace with your Arduino's IP address
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func sprinklerOnButtonPressed(_ sender: Any) {
+        sendHTTPRequest(endpoint: "/on")
+    }
+    
+    @IBAction func sprinklerOffButtonPressed(_ sender: Any) {
+        sendHTTPRequest(endpoint: "/off")
+    }
+    
+    // Example: An outlet for a label (if you have one)
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    // Function to send HTTP requests to the Arduino
+        func sendHTTPRequest(endpoint: String) {
+            guard let url = URL(string: arduinoIP + endpoint) else { return }
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print("Error: \(error)")
+                    return
+                }
+                if let data = data, let responseString = String(data: data, encoding: .utf8) {
+                    print("Response: \(responseString)")
+                    DispatchQueue.main.async {
+                        self.statusLabel.text = responseString // Update UI if needed
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
--- src/raindance/raindance.ino
+++ src/raindance/raindance.ino
@@ -1,17 +1,13 @@
 #include <WiFiNINA.h>
-#include <PubSubClient.h>
+#include <WiFiServer.h>
 #include <EEPROM.h>
 
 // WiFi settings
 const char* ssid = "ARRIS-439E";
 const char* password = "287860664144";
 
-// MQTT settings
-const char* mqtt_server = "47c8a5f0e323493b8ad39b814771ba74.s1.eu.hivemq.cloud";
-const char* mqtt_topic = "irrigation/control";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
 
 // Define the pin for the relay
 const int relayPin = 7;
@@ -20,7 +16,7 @@ void setup() {
   // put your setup code here, to run once:
   
   Serial.begin(9600);
-  delay(1000);
+  delay(500);
   while (!Serial) {
     ; // wait for SERIAL PORT TO CONNECT
   }
@@ -28,92 +24,23 @@ void setup() {
 
   // Initialize the relay pin as an output
   pinMode(relayPin, OUTPUT);
-  Serial.println("initializing digital output pin for relay");
+  Serial.println("configuring digital output pin for relay");
 
   // Ensure the relay is off initially
   digitalWrite(relayPin, LOW);
 
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, password);
+    delay(10000);
+  }
+  server.begin();  
+  IPAddress myIPAddress = WiFi.localIP();
+  Serial.print("Connected to WiFi with address ");
+  Serial.println(myIPAddress);
 }
 
-void setup_wifi() {
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  // check for the presence of the shield:
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
-    // don't continue:
-    while (true);
-  }
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    
-    int wifiStatus = WiFi.status();
-    Serial.println(wifiStatus);
-    if (wifiStatus == WL_NO_SHIELD) { Serial.println("wifiStatus = WL_NO_SHIELD");}
-    if (wifiStatus == WL_CONNECTED) { Serial.println("wifiStatus = WL_CONNECTED");}
-    if (wifiStatus == WL_IDLE_STATUS) { Serial.println("wifiStatus = WL_IDLE_STATUS");}
-    if (wifiStatus == WL_AP_CONNECTED) { Serial.println("wifiStatus = WL_AP_CONNECTED");}
-    if (wifiStatus == WL_AP_LISTENING) { Serial.println("wifiStatus = WL_AP_LISTENING");}
-    if (wifiStatus == WL_CONNECT_FAILED) { Serial.println("wifiStatus = WL_CONNECT_FAILED");}
-    if (wifiStatus == WL_CONNECTION_LOST) { Serial.println("wifiStatus = WL_CONNECTION_LOST");}
-
-    Serial.println(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
-  String messageTemp;
-  
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-  }
-  Serial.println();
-}
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.println("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("ArduinoClient", "williamlaing", "Laingwe7&04")) {
-      Serial.println("MQTT client connected");
-      // Once connected, publish an announcement...
-      Serial.println("MQTT client preparing to publish ...");
-      if (!client.publish("outTopic", "hello world")) { 
-        Serial.println("MQTT client could NOT publish");
-      }
-      // ... and resubscribe
-      Serial.println("MQTT client preparing to subscribe ...");
-      if (!client.subscribe("inTopic")) { 
-        Serial.println("MQTT client could NOT subscribe");
-      }
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -128,11 +55,59 @@ void loop() {
   // digitalWrite(relayPin, LOW);
   // delay(60000); // Wait for 30 seconds (30,000 milliseconds)
 
-  if (!client.connected()) {
-    Serial.println("client not connected");
+  WiFiClient client = server.available();
+  while (!client) {
+    Serial.println("http server not available");
     delay(5000);
-    reconnect();
+    client = server.available();
   }
-  client.loop();
+  if (client) {
+    Serial.println("New client connected");
+    String currentLine = "";
+    while (client.connected()) {
+      //Serial.println("New client still connected");
+      if (client.available()) {
+        Serial.println("New client connected");
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n') {
+          Serial.println(">>>>>>>>>> \n detetced! <<<<<<<<<<<<");
+          Serial.print(">>>>>>>>>> currentLine.length() <<<<<<<<<<<<");
+          Serial.println(currentLine.length());
+          Serial.println(currentLine);
 
+          if (currentLine.length() != 0) {
+            // Send HTTP response
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+            // Handle request
+            if (client.available()) {
+              Serial.println(" ********* client ready to write to relay *********");
+              String request = client.readStringUntil('\r');
+              Serial.println(request);
+              if (request.indexOf("/on") != -1) {
+                digitalWrite(relayPin, HIGH);
+                client.print("Sprinkler is ON");
+              } else if (request.indexOf("/off") != -1) {
+                digitalWrite(relayPin, LOW);
+                client.print("Sprinkler is OFF");
+              }
+            }
+            client.println();
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+      }
+    }
+    client.stop();
+    Serial.println("Client disconnected");
+  } else {
+    Serial.println("New client NOT connected");
+    delay(3000);
+  }
 }
